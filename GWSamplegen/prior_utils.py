5b107085e9d57c936abba2436875a714aa21e900
Alistair <Eedles@github.com>
2025-08-04 14:04:04 +1000

    Adding prior and template utility files

---
+++ GWSamplegen/prior_utils.py
@@ -0,0 +1,140 @@
+import numpy as np
+import bilby.core.prior
+from bilby.core.prior import (
+    Cosine,
+    PowerLaw,
+    PriorDict,
+    Sine,
+    Uniform,
+    Triangular,
+)
+from typing import Union
+from bilby.gw.prior import UniformComovingVolume, UniformSourceFrame
+
+def constructPrior(
+	prior: Union[Uniform, Cosine, UniformComovingVolume, PowerLaw, UniformSourceFrame], 
+	min: float, 
+	max: float,
+	**kwargs
+) -> PriorDict:
+	#generic constructor for bilby priors. 
+
+	# if prior == PowerLaw:
+	#     kwargs['alpha'] = powerlaw_alpha
+
+	if max <= min:
+		return max
+	else:
+		try:
+			return prior(minimum = min, maximum = max, **kwargs)
+		except:
+			return prior(minimum = min, maximum = max)
+		
+def inv_tri_bilby(mode,minimum,maximum,val):
+	#copied from Bilby's triangular prior
+	scale = maximum - minimum
+	fractional_mode = (mode - minimum) / (maximum - minimum)
+
+	below_mode = (val * scale * (mode - minimum)) ** 0.5
+	above_mode = ((1 - val) * scale * (maximum - mode)) ** 0.5
+	return (minimum + below_mode) * (val < fractional_mode) + (
+		maximum - above_mode
+	) * (val >= fractional_mode)
+
+def tri_uniform(a,b, mode, r):
+	#r = probability of drawing from triangular distribution
+	#if r = 0, we draw from a uniform distribution
+	#if r = 1, we draw from a triangular distribution
+	p = np.random.uniform(0, 1)
+	if p < r:
+		return np.random.uniform(a,b)
+	else:
+		return inv_tri_bilby(mode, a, b, np.random.uniform(0, 1))
+
+class TriUniform(bilby.core.prior.Prior):
+	""" A combination of a uniform and triangular distribution
+	
+	Parameters
+	----------
+	minimum: float
+		Minimum of the distribution
+	maximum: float
+		Maximum of the distribution
+	mode: float	
+		Mode of the triangular distribution
+	r: float	
+		Probability of drawing from the triangular distribution. With the default value of 2/3,
+        the mode has twice the probability density of the uniform distribution.
+	
+	"""
+	def __init__(self, minimum, maximum, mode, r = 2/3, name=None, latex_label=None, unit=None, boundary=None):
+		self.mode = mode
+		self.r = r
+		super(TriUniform, self).__init__(name=name, latex_label=latex_label, minimum=minimum, maximum=maximum, unit=unit, boundary=boundary)
+
+	def rescale(self, val):
+		return tri_uniform(self.minimum, self.maximum, self.mode, self.r)
+
+	def sample(self, size=None):
+		if size is None:
+			return self.rescale(np.random.uniform(0, 1))
+		else:
+			return np.array([self.rescale(np.random.uniform(0, 1)) for i in range(size)])
+
+	def __repr__(self):
+		return f'TriUniform(minimum={self.minimum}, maximum={self.maximum}, mode={self.mode}, r={self.r})'
+
+
+#construct a prior dictionary and set the priors for each parameter TODO: move to utils
+def sample_masses_from_cm_q(parameters):
+	converted = parameters.copy()
+	converted['mass1_source'], converted['mass2_source'] = bilby.gw.conversion.chirp_mass_and_mass_ratio_to_component_masses(parameters['chirp_mass'], parameters['mass_ratio'])
+	#print(converted)
+	return converted
+
+# Function from LVC Rates & Populations Group
+def powerlaw_setup(minv, maxv, alpha):
+    a = (maxv / minv) ** (alpha + 1.) - 1.
+    b = 1. / (alpha + 1.)
+    return a, b
+
+
+# Function from LVC Rates & Populations Group
+def powerlaw_sample(x_rand, minv, a, b):
+    return minv * (1. + a * x_rand) ** b
+
+
+# Function from LVC Rates & Populations Group
+def draw_mass_pair_power(m1_min, m1_max, m2_min, m2_max, m1pow, m2pow):
+    a1, b1 = powerlaw_setup(m1_min, m1_max, m1pow)
+    while True:
+        x1 = np.random.random()
+        x2 = np.random.random()
+        m1 = powerlaw_sample(x1, m1_min, a1, b1)
+        if m2_max < m1:
+            a2, b2 = powerlaw_setup(m2_min, m2_max, m2pow)
+        else:
+            a2, b2 = powerlaw_setup(m2_min, m1, m2pow)
+        m2 = powerlaw_sample(x2, m2_min, a2, b2)
+        yield m1, m2
+
+
+
+# Function from LVC Rates & Populations Group
+def draw_spin_isotropic(spin_dict):
+    '''
+    Yields a random spin tuple (s_x, s_y, s_z) isotropically
+    distributed with uniform magnitude distribution.
+    '''
+    max_spin = spin_dict
+
+    while True:
+        s = np.random.uniform(-1., 1., size=3)
+        ssq = sum(s ** 2.)
+
+        # s is a vector uniformly distributed inside the unit sphere
+        # p(|s|) ~ |s|^2
+        if ssq < 1.:
+            # s * |s|^2 has magnitude |chi| = |s|^3
+            # p(|chi|) d|chi| = p(|s|) d|s| ~ |s|^2 d|s| ~ const. d|chi|
+            return s * ssq * max_spin
---
+++ GWSamplegen/template_utils.py
@@ -0,0 +1,100 @@
+from GWSamplegen.waveform_utils import maximum_f_lower, t_at_f, select_approximant, fast_point_distance
+from pycbc.waveform import get_td_waveform, get_fd_waveform, td_approximants, fd_approximants
+import numpy as np
+from pycbc.filter import matchedfilter
+import gc
+
+def cfac(e):
+    #a correction factor: when searching for circular templates for an eccentric waveform,
+    #adjust the template mass by this factor. Tested on BNS signals.
+    return e/20 + 1
+
+
+def find_templates(waveform, args, futile_limit = 10, match_target = 0.9, required_templates = 10, acceptable_thresh = 10):
+    #waveform is a dictionary containing the waveform parameters
+
+    #print("f_lower: ", f_lower)
+    #used to be parameters, templates, psd, futile_limit = 10, match_target = 0.9, required_templates = 10, acceptable_thresh = 10
+    olaps = []
+
+    if "eccentricity" in waveform:
+        #a = (eccentric_masses_to_circular(waveform['eccentricity']) -1) /8 + 1
+        a = cfac(waveform['eccentricity'])
+    else: 
+        a = 1
+
+    idx = np.argsort(fast_point_distance(args["aXis"], (waveform['mass1'] * a, waveform['mass2'] * a, 
+                                            waveform['spin1z'], waveform['spin2z']), args["metricParams"]))
+    
+    temp_td_approximant = select_approximant(waveform['mass1'], waveform['mass2'], args["td_approximant"], domain='time')
+
+    #note: approximant for hp_f was IMRPhenomPv2
+    #this waveform should be fast FD waveform that's accurate (for BBH that's IMRPhenomPv2)
+    #hp_f, _ = get_fd_waveform(mass1 = waveform['mass1'], mass2 = waveform['mass2'],
+    #        spin1z = waveform['spin1z'], spin2z = waveform['spin2z'],
+    #        f_lower = 10, delta_f = 1/1024, f_final = 2048*4, approximant = waveform_approximant)
+
+    #temp_f_lower = min(10, maximum_f_lower(waveform['mass1'], waveform['mass2']))
+    if temp_td_approximant != "SpinTaylorT4" and t_at_f(waveform['mass1'], waveform['mass2'], 10) > 100:
+        temp_f_lower = 20
+    else:
+        temp_f_lower = 10
+    temp_f_lower = min(temp_f_lower, maximum_f_lower(waveform['mass1'], waveform['mass2']))
+
+    if temp_td_approximant in ["TaylorF2Ecc", "EccentricFD", "EccentricTD"]:
+        print("Eccentric approximant detected. Using f_lower = 20 Hz.")
+        temp_f_lower = 20
+
+    hp = None
+    if temp_td_approximant in td_approximants():
+        hp,_ = get_td_waveform(approximant=temp_td_approximant, mass1=waveform['mass1'], mass2=waveform['mass2'],
+                            spin1x=waveform['spin1x'], spin2x=waveform['spin2x'],
+                            spin1y=waveform['spin1y'], spin2y=waveform['spin2y'],
+                            spin1z=waveform['spin1z'], spin2z=waveform['spin2z'],
+                            f_lower=temp_f_lower, delta_t=1/(8*2048))
+        hp = hp.resample(1/2048)
+
+        if -hp.sample_times[0] > args["duration"]:
+            print("shortening sample")
+            chop = np.argmin(np.abs(hp.sample_times + args["duration"]-10))
+            hp.start_time = hp.sample_times[chop]
+            hp.data = hp.data[chop:]
+            print("New sample delta f is ", 1/hp.delta_f)
+
+        #print("trying to convert to frequency series")
+        hp_f = hp.to_frequencyseries(delta_f=1/args["duration"])
+
+    elif args["td_approximant"] not in td_approximants() and args["td_approximant"] in fd_approximants():
+        print("TD approximant is FD only. No need to convert.")
+        hp_f, _ = get_fd_waveform(waveform, approximant = args["td_approximant"], f_lower = temp_f_lower, delta_f = 1/args["duration"])
+
+    for j in range(futile_limit):
+        #note: approximant for hpt_f was SEOBNRv4_ROM
+        #this waveform should be the template bank waveform. Should NOT include precession etc. 
+        #unless the bank was generated with it.
+        temp_fd_approximant = select_approximant(args["template_bank_params"][idx[j]][1], args["template_bank_params"][idx[j]][2], args["fd_approximant"], domain='frequency')
+        hpt_f, _ = get_fd_waveform(mass1 = args["template_bank_params"][idx[j]][1], mass2 = args["template_bank_params"][idx[j]][2],
+                                spin1z = args["template_bank_params"][idx[j]][3], spin2z = args["template_bank_params"][idx[j]][4],
+                                f_lower = temp_f_lower, delta_f = 1/1024, f_final = 2048*4, approximant = temp_fd_approximant)
+        
+        hpt_f.resize(len(hp_f))
+        olap = matchedfilter.match(hp_f, hpt_f, psd=args["psds"]['L1'], low_frequency_cutoff=args["f_lower"], high_frequency_cutoff=1024)[0]
+        #olap = get_overlap_nsbh_mp((hp_f, args["template_bank_params"][idx[j]], args["template_bank_params"][idx[j],5]))
+        olaps.append(olap)
+        #if olap > match_target:
+        #    print("found a match for injection ", n)
+        del hpt_f
+
+        if np.max(olaps) > match_target and j == acceptable_thresh:
+            #print("found good enough matches for injection ", n, "best was ", np.max(olaps))
+            break
+    olaps = np.array(olaps)
+    if np.max(olaps) < match_target:
+        print("failed to find a good match for injection. best was ", np.max(olaps), ". m1, m2 were: ", waveform['mass1'], waveform['mass2'])
+    else:
+        print("found a good match for injection. best was ", np.max(olaps))
+    #argsort the olaps and return the template ids
+    del hp_f, hp
+    #run garbage collection
+    gc.collect()
+    return olaps[np.argsort(olaps)[::-1][:required_templates]], idx[np.argsort(olaps)[::-1][:required_templates]] 
