6cc4f5fa9a86c550afe383199c2df813f2cbafb0
Alistair <alistair-mcleod@github.com>
2026-03-30 18:20:40 +1100

    adjusting default behaviour + adding jobs as an argument

--- share/SNR_np.sh
+++ share/SNR_np.sh
@@ -2,13 +2,19 @@
 #SBATCH --output=./logs/%x_%a.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --time=20:00:00
-#SBATCH --mem=160gb
-#SBATCH --array=0-19
+#SBATCH --time=50:00:00
+#SBATCH --mem=80gb
+#SBATCH --array=0-39
 
 #this script should be run using the generate_configs_workflow.sh script
 
 config_file=$1
-
-#$SLURM_ARRAY_TASK_COUNT
-python ${GWSAMPLEGEN_DIR}/share/asyncSNR_np.py --index=$SLURM_ARRAY_TASK_ID --totaljobs=$SLURM_ARRAY_TASK_COUNT --config-file=$config_file
+#check if there's a second argument
+if [ -z "$2" ]; then
+	n_jobs=$SLURM_ARRAY_TASK_COUNT
+else
+	n_jobs=$2
+fi
+#${SLURM_ARRAY_TASK_COUNT}
+#TODO: make the number of jobs a parameter
+python ${GWSAMPLEGEN_DIR}/share/asyncSNR_np.py --index=$SLURM_ARRAY_TASK_ID --totaljobs=$n_jobs --config-file=$config_file
